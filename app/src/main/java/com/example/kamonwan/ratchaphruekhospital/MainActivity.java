e88cbbdd8ee7c78639128e16f2c03a62cc93dd07
kamonwans <kamonwan294@gmail.com>
2017-07-13 17:04:00 +0700

    change string

--- app/src/main/java/com/example/kamonwan/ratchaphruekhospital/MainActivity.java
+++ app/src/main/java/com/example/kamonwan/ratchaphruekhospital/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.kamonwan.ratchaphruekhospital;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
@@ -18,16 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initInstance();
 
+        initInstance();
     }
 
     private void initInstance() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
-                drawerLayout, R.string.oper_drawer, R.string.close_drawer);
+                drawerLayout, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
     }
@@ -36,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
-
     }
 
     @Override
