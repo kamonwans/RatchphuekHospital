6861aa4b128ddfd5c3443c312eedd72c07ed17a3
kamonwans <kamonwan294@gmail.com>
2017-07-18 20:02:59 +0700

    change text

--- app/src/main/java/com/example/kamonwan/ratchaphruekhospital/EditProfileActivity.java
+++ app/src/main/java/com/example/kamonwan/ratchaphruekhospital/EditProfileActivity.java
@@ -7,8 +7,10 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import java.io.IOException;
 
@@ -25,7 +27,31 @@ public class EditProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
 
         initInstance();
+        FragmentSpinner();
 
+    }
+
+    private void FragmentSpinner() {
+
+        Spinner dropdownEdit = (Spinner) findViewById(R.id.btn_dropdownEdit);
+        Spinner dropdownEditDate = (Spinner) findViewById(R.id.btn_dropdownEditDate);
+        Spinner dropdownEditMount = (Spinner) findViewById(R.id.btn_dropdownEditMount);
+        Spinner dropdownEditYear = (Spinner) findViewById(R.id.btn_dropdownEditYear);
+
+        String[] item = new String[]{"นาย", "นาง", "นางสาว"};
+        String[] itemDate = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
+                "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+        String[] itemMount = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",};
+        String[] itemYear = new String[]{"2555", "2556", "2557", "2558", "2559", "2560", "2561",};
+
+        ArrayAdapter<String> adapterEdit = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, item);
+        ArrayAdapter<String> adapterEditDate = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, itemDate);
+        ArrayAdapter<String> adapterEditMount = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, itemMount);
+        ArrayAdapter<String> adapterEditYear = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, itemYear);
+        dropdownEdit.setAdapter(adapterEdit);
+        dropdownEditDate.setAdapter(adapterEditDate);
+        dropdownEditMount.setAdapter(adapterEditMount);
+        dropdownEditYear.setAdapter(adapterEditYear);
     }
 
     private void initInstance() {
