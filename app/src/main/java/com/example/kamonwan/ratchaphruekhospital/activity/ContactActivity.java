09803ae1d93a00f48e5d110c998331baa82218cd
kamonwans <kamonwan294@gmail.com>
2017-07-21 10:17:17 +0700

    Edit Contact

--- app/src/main/java/com/example/kamonwan/ratchaphruekhospital/activity/ContactActivity.java
+++ app/src/main/java/com/example/kamonwan/ratchaphruekhospital/activity/ContactActivity.java
@@ -1,36 +1,15 @@
 package com.example.kamonwan.ratchaphruekhospital.activity;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.example.kamonwan.ratchaphruekhospital.R;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ContactActivity extends FragmentActivity {
-
+public class ContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-
     }
-
-
 }
