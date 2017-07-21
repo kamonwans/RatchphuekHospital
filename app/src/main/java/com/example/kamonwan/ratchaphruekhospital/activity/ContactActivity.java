e675aa1c5c59484b3374ce4d8c2f4ea162d678d0
kamonwans <kamonwan294@gmail.com>
2017-07-21 11:53:15 +0700

    Gps

--- app/src/main/java/com/example/kamonwan/ratchaphruekhospital/activity/ContactActivity.java
+++ app/src/main/java/com/example/kamonwan/ratchaphruekhospital/activity/ContactActivity.java
@@ -1,9 +1,11 @@
 package com.example.kamonwan.ratchaphruekhospital.activity;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.kamonwan.ratchaphruekhospital.R;
+import com.example.kamonwan.ratchaphruekhospital.model.MainFragment;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
     }
+
 }
